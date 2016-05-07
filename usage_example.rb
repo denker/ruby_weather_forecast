2421bd2192325de7bc72b7756d9cc67ab46c7049
Tim Zuev <tim.zuev@gmail.com>
2016-05-07 04:05:29 +0300

    removed awesome_print dependency from usage_example

--- usage_example.rb
+++ usage_example.rb
@@ -1,4 +1,6 @@
-require 'ap'
 require './forecast.rb'
 
-ap f = Forecast.get_weather_for('00688', Time.new(2016,3,25,15,0).to_i)
+f = Forecast.get_weather_for('00688', Time.new(2016,3,25,15,0).to_i)
+
+puts "Coordinated: #{f['latitude']}, #{f['longitude']}"
+f['currently'].each_pair {|k,v| puts "#{k}: #{v}"}
